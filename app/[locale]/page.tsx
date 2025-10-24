c6317ff9161d5aefbe99b19939eee54dd91c55b6
Homayoun <homayoun763@gmail.com>
2025-10-24 17:41:35 +0330

    setup next-intl for multi language support

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,23 @@
+import { useTranslations } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
+import { use } from "react";
+
+export default function HomePage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = use(params);
+
+  // Enable static rendering for this page
+  setRequestLocale(locale);
+
+  const t = useTranslations("HomePage");
+
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      {/* Your page content */}
+    </div>
+  );
+}
